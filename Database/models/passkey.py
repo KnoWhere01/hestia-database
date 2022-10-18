50ae8521364d0b5869830341c74ace96e135612e
Tajikarao <tajikarao@protonmail.com>
2022-10-19 00:29:02 +0200

    passkey: uuid4 have to be unique

--- Database/models/passkey.py
+++ Database/models/passkey.py
@@ -6,6 +6,6 @@ from utils.base import Base
 class PassKey(Base):
     __tablename__ = "passkeys"
     id = Column(Integer, primary_key=True)
-    passkey = Column(String(36))
+    passkey = Column(String(36), unique=True)
     torrent = Column(Integer, ForeignKey("torrents.id"))
     user = Column(Integer, ForeignKey("users.id"))
