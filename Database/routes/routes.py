ab4f315ac2411255e17b9e6862b44e8d5ca84712
タジカラオ <103996576+Tajikarao@users.noreply.github.com>
2022-10-20 15:31:18 +0200

    routes: update engine to support split CRUD

--- Database/routes/routes.py
+++ Database/routes/routes.py
@@ -5,9 +5,12 @@ from os import scandir
 def set_routes(app):
     for item in scandir("routes"):
         if item.is_dir() and "__pycache__" not in item.name:
-            locals()[item.name] = importlib.import_module(
-                f"routes.{item.name}.{item.name}"
-            )
-            app.register_blueprint(getattr(locals()[item.name], "blueprint"))
+            for file in ["create", "delete", "read", "update"]:
+                locals()[f"{item.name}_{file}"] = importlib.import_module(
+                    f"routes.{item.name}.{file}"
+                )
+                app.register_blueprint(
+                    getattr(locals()[f"{item.name}_{file}"], "blueprint")
+                )
 
     return True
