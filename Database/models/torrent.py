7d64e2641e31e202aaaab3ecfcfad0129ccb82de
Tajikarao <tajikarao@protonmail.com>
2022-10-23 21:16:47 +0200

    torrent: add uploader link & torrent relation

--- Database/models/torrent.py
+++ Database/models/torrent.py
@@ -1,12 +1,15 @@
-from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.dialects.mysql import BIGINT, TINYINT
+from sqlalchemy.orm import relationship
 
+from models.user import User
 from utils.base import Base
 
 
 class Torrent(Base):
     __tablename__ = "torrents"
     id = Column(Integer, primary_key=True)
+    uploader = Column(Integer, ForeignKey("users.id"))
     info_hash = Column(String(40))
     name = Column(String(255))
     desc = Column(String(1000))
@@ -17,3 +20,5 @@ class Torrent(Base):
     seeders = Column(BIGINT(unsigned=True), default=0)
     leechers = Column(BIGINT(unsigned=True), default=0)
     last_checked = Column(DateTime)
+
+    relationship(User, backref="torrents")
