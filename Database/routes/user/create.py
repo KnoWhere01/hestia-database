47ef9baf9af3bd5801cfd7cffee4f74f2ff098f8
Tajikarao <tajikarao@protonmail.com>
2022-10-26 00:36:10 +0200

    user: update create

--- Database/routes/user/create.py
+++ Database/routes/user/create.py
@@ -1,10 +1,34 @@
 from flask import request
+
+from models.user import User
 from utils.blueprint import Blueprint
+from utils.sqlalchemy import SQLAlchemy
 
 blueprint = Blueprint("user_create")
+
+database: SQLAlchemy = SQLAlchemy()
 
 
 @blueprint.route("/user", methods=["POST"])
 def user_create():
     """User CREATE"""
-    return {"method": "POST", "body": request.json}
+
+    username = request.json.get("username")
+    password = request.json.get("password")
+    uploaded = 0
+    downloaded = 0
+
+    user = User(
+        username=username,
+        password=password,
+        uploaded=uploaded,
+        downloaded=downloaded,
+    )
+
+    if database.add(user) and database.commit():
+        return {
+            "succes": True,
+            "user": {"username": username},
+        }, 200
+
+    return {"succes": False}, 403
