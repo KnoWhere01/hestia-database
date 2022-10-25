91b5ccc0ea9cbbd51b254eba1fa60d2b26e2c2a6
タジカラオ <103996576+Tajikarao@users.noreply.github.com>
2022-10-25 17:03:33 +0200

    passkey: prepare creation

--- Database/routes/passkey/create.py
+++ Database/routes/passkey/create.py
@@ -1,10 +1,29 @@
+import uuid
+
 from flask import request
+from models.passkey import PassKey
 from utils.blueprint import Blueprint
+from utils.sqlalchemy import SQLAlchemy
 
 blueprint = Blueprint("passkey_create")
+
+database: SQLAlchemy = SQLAlchemy()
 
 
 @blueprint.route("/passkey", methods=["POST"])
 def passkey_create():
     """Passkey CREATE"""
-    return {"method": "POST", "body": request.json}
+
+    passkey_uuid = uuid.uuid4()
+    torrent = request.json.get("torrent")
+    user = request.json.get("user")
+
+    passkey = PassKey(passkey_uuid, torrent, user)
+
+    if database.add(passkey) and database.commit():
+        return {
+            "succes": True,
+            "PassKey": {"passkey": passkey_uuid, "torrent": passkey_uuid, "user": user},
+        }, 200
+
+    return {"succes": False}, 403
