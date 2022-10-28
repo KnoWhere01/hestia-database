63e623e5f541e9803dc34bd832b26b82c6925492
タジカラオ <103996576+Tajikarao@users.noreply.github.com>
2022-10-28 12:04:53 +0200

    user_read: refacto

--- Database/routes/user/read.py
+++ Database/routes/user/read.py
@@ -1,9 +1,30 @@
+from flask import request
+
 from utils.blueprint import Blueprint
+from utils.response import Response
+from utils.sqlalchemy import SQLAlchemy
+
+from html import escape
 
 blueprint = Blueprint("user_read")
+
+database: SQLAlchemy = SQLAlchemy()
 
 
 @blueprint.route("/user/<user>", methods=["GET"])
 def user_read(user):
     """User READ"""
-    return {"method": "GET", "return": user}
+
+    if username := escape(user):
+        if user := database.query("User").filter_by(username=username).limit(1).first():
+            return Response.success(
+                message={
+                    "username": user.username,
+                    "uploaded": user.uploaded,
+                    "downloaded": user.downloaded,
+                }
+            )
+
+        return Response.error(message="Username unavailable.")
+
+    return Response.error(message="Username unavailable.")
