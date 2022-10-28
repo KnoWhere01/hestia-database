a8a41b0cb5661b2df423ffa80f04d230afa06aed
タジカラオ <103996576+Tajikarao@users.noreply.github.com>
2022-10-28 11:35:22 +0200

    user_delete: refacto

--- Database/routes/user/delete.py
+++ Database/routes/user/delete.py
@@ -1,9 +1,25 @@
+from flask import request
+
 from utils.blueprint import Blueprint
+from utils.response import Response
+from utils.sqlalchemy import SQLAlchemy
 
 blueprint = Blueprint("user_delete")
 
+database: SQLAlchemy = SQLAlchemy()
 
-@blueprint.route("/user/<user>", methods=["DELETE"])
-def user_delete(user):
+
+@blueprint.route("/user", methods=["DELETE"])
+def user_delete():
     """User DELETE"""
-    return {"method": "DELETE", "delete": user}
+
+    if username := request.json.get("username", False):
+        if user := database.query("User").filter_by(username=username):
+            if user.delete() and database.commit():
+                return Response.success(message="Account successfully deleted.")
+
+            return Response.error(message="Error during account deletion.")
+
+        return Response.error(message="Username unavailable.")
+
+    return Response.error(message="Error during account deletion.")
