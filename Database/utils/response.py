f67381933d6ce1baba11cbea448da51635c9ab88
タジカラオ <103996576+Tajikarao@users.noreply.github.com>
2022-10-28 13:00:58 +0200

    utils.response: accept dict as message

--- Database/utils/response.py
+++ Database/utils/response.py
@@ -1,16 +1,19 @@
+from typing import Union
+
 from utils.singleton import Singleton
+
 
 class Response(object, metaclass=Singleton):
     @staticmethod
-    def success(message: str, code: int = 200):
+    def success(message: Union[str, dict], code: int = 200):
         return {
-        "success": True,
-        "message": message,
-    }, code
-    
+            "success": True,
+            "message": message,
+        }, code
+
     @staticmethod
-    def error(message: str, code: int = 403):
+    def error(message: Union[str, dict], code: int = 403):
         return {
-        "success": False,
-        "message": message,
-    }, code
+            "success": False,
+            "message": message,
+        }, code
