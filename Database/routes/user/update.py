173a6baa3ab46ca11b1e4e2a88932600d3642ef0
タジカラオ <103996576+Tajikarao@users.noreply.github.com>
2022-10-28 13:47:18 +0200

    user_update: refacto

--- Database/routes/user/update.py
+++ Database/routes/user/update.py
@@ -1,9 +1,46 @@
+from html import escape
+
+from flask import request
+from passlib.hash import bcrypt
+
 from utils.blueprint import Blueprint
+from utils.response import Response
+from utils.sqlalchemy import SQLAlchemy
 
 blueprint = Blueprint("user_update")
+
+database: SQLAlchemy = SQLAlchemy()
 
 
 @blueprint.route("/user/<user>", methods=["PATCH", "PUT"])
 def user_update(user):
     """User UPDATE"""
-    return {"method": ["PATCH", "PUT"], "return": user}
+    if username := escape(user):
+        if user := database.query("User").filter_by(username=username):
+            password = request.json.get("password", False)
+            uploaded = request.json.get("uploaded", 0)
+            downloaded = request.json.get("downloaded", 0)
+
+            if password:
+                user.update({"password": bcrypt.hash(password)})
+
+            if uploaded:
+                user.update({"uploaded": uploaded})
+
+            if downloaded:
+                user.update({"downloaded": downloaded})
+
+            if database.commit():
+                user = user.limit(1).first()
+
+                return Response.success(
+                    message={
+                        "username": user.username,
+                        "uploaded": user.uploaded,
+                        "downloaded": user.downloaded,
+                    }
+                )
+
+        return Response.error(message="Username unavailable.")
+
+    return Response.error(message="Error during account update.")
