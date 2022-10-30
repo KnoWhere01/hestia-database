47390aae765f623bee8bffb4a885f99ef88307cc
Tajikarao <tajikarao@protonmail.com>
2022-10-31 00:15:51 +0100

    passkey_delete: refacto

--- Database/routes/passkey/delete.py
+++ Database/routes/passkey/delete.py
@@ -1,9 +1,24 @@
+from flask import request
 from utils.blueprint import Blueprint
+from utils.response import Response
+from utils.sqlalchemy import SQLAlchemy
 
 blueprint = Blueprint("passkey_delete")
 
+database: SQLAlchemy = SQLAlchemy()
 
-@blueprint.route("/passkey/<passKey>", methods=["DELETE"])
-def passkey_delete(passKey):
-    """Passkey DELETE"""
-    return {"method": "DELETE", "delete": passKey}
+
+@blueprint.route("/passkey", methods=["DELETE"])
+def user_delete():
+    """User DELETE"""
+
+    if passkey := request.json.get("passkey", False):
+        if passkey := database.query("PassKey").filter_by(passkey=passkey):
+            if passkey.delete() and database.commit():
+                return Response.success(message="Passkey successfully deleted.")
+
+            return Response.error(message="Error during passkey deletion.")
+
+        return Response.error(message="Passkey unavailable.")
+
+    return Response.error(message="Error during account deletion.")
