ed2ef4b4131b48e7e5be134880a5b7c2d62afa6d
タジカラオ <103996576+Tajikarao@users.noreply.github.com>
2022-10-31 11:59:59 +0100

    torrent_create: refacto

--- Database/routes/torrent/create.py
+++ Database/routes/torrent/create.py
@@ -1,7 +1,8 @@
 from flask import request
-
 from models.torrent import Torrent
+from datetime import date
 from utils.blueprint import Blueprint
+from utils.response import Response
 from utils.sqlalchemy import SQLAlchemy
 
 blueprint = Blueprint("torrent_create")
@@ -13,36 +14,44 @@ database: SQLAlchemy = SQLAlchemy()
 def torrent_create():
     """Torrent CREATE"""
 
-    uploader = request.json.get("uploader")
-    info_hash = request.json.get("info_hash")
-    name = request.json.get("name")
-    desc = request.json.get("desc")
-    torrent_file = request.json.get("torrent_file")
-    torrent_version = request.json.get("torrent_version")
-    uploaded_time = request.json.get("uploaded_time")
-    download_count = request.json.get("download_count")
-    seeders = request.json.get("seeders")
-    leechers = request.json.get("leechers")
-    last_checked = request.json.get("last_checked")
+    current_date = date.today()
 
-    torrent = Torrent(
-        uploader=uploader,
-        info_hash=info_hash,
-        name=name,
-        desc=desc,
-        torrent_file=torrent_file,
-        torrent_version=torrent_version,
-        uploaded_time=uploaded_time,
-        download_count=download_count,
-        seeders=seeders,
-        leechers=leechers,
-        last_checked=last_checked,
-    )
+    uploader = request.json.get("uploader", False)
+    info_hash = request.json.get("info_hash", False)
+    name = request.json.get("name", False)
+    desc = request.json.get("desc", False)
+    torrent_file = request.json.get("torrent_file", False)
+    torrent_version = request.json.get("torrent_version", False)
+    uploaded_time = current_date
+    seeders, leechers, download_count = 0, 0, 0
+    last_checked = current_date
 
-    if database.add(torrent) and database.commit():
-        return {
-            "succes": True,
-            "torrent": "",
-        }, 200
+    if (
+        uploader
+        and info_hash
+        and name
+        and desc
+        and torrent_file
+        and torrent_version
+    ):
+        if not (database.query("Torrent").filter_by(info_hash=info_hash).limit(1).first()):
+            torrent = Torrent(
+                uploader=uploader,
+                info_hash=info_hash,
+                name=name,
+                desc=desc,
+                torrent_file=torrent_file,
+                torrent_version=torrent_version,
+                uploaded_time=uploaded_time,
+                download_count=download_count,
+                seeders=seeders,
+                leechers=leechers,
+                last_checked=last_checked
+            )
 
-    return {"succes": False}, 403
+            if database.add(torrent) and database.commit():
+                return Response.success(message="Torrent created successfully.")
+
+        return Response.error(message="Torrent unavailable.")
+
+    return Response.error(message="Error during torrent creation.")
