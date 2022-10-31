44bb8e20f7d34a16d3bb89a44c5b2d1e632ecb29
タジカラオ <103996576+Tajikarao@users.noreply.github.com>
2022-10-31 13:52:33 +0100

    torrent_delete: refacto

--- Database/routes/torrent/delete.py
+++ Database/routes/torrent/delete.py
@@ -1,9 +1,25 @@
+from flask import request
+
 from utils.blueprint import Blueprint
+from utils.response import Response
+from utils.sqlalchemy import SQLAlchemy
 
 blueprint = Blueprint("torrent_delete")
 
+database: SQLAlchemy = SQLAlchemy()
 
-@blueprint.route("/torrent/<torrent>", methods=["DELETE"])
-def torrent_delete(torrent):
+
+@blueprint.route("/torrent", methods=["DELETE"])
+def torrent_delete():
     """Torrent DELETE"""
-    return {"method": "DELETE", "delete": torrent}
+
+    if info_hash := request.json.get("info_hash", False):
+        if torrent := database.query("Torrent").filter_by(info_hash=info_hash):
+            if torrent.delete() and database.commit():
+                return Response.success(message="Torrent successfully deleted.")
+
+            return Response.error(message="Error during torrent deletion.")
+
+        return Response.error(message="Torrent unavailable.")
+
+    return Response.error(message="Error during torrent deletion.")
