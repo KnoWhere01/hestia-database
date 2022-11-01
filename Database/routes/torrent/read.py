23d239d0b9ec1126a2afbdd44d9e05c19caf9b68
Tajikarao <tajikarao@protonmail.com>
2022-11-02 00:28:49 +0100

    torrent_read: refacto

--- Database/routes/torrent/read.py
+++ Database/routes/torrent/read.py
@@ -1,9 +1,38 @@
+from flask import request
+
 from utils.blueprint import Blueprint
+from utils.response import Response
+from utils.sqlalchemy import SQLAlchemy
+
+from html import escape
 
 blueprint = Blueprint("torrent_read")
 
+database: SQLAlchemy = SQLAlchemy()
 
-@blueprint.route("/torrent/<torrent>", methods=["GET"])
-def torrent_read(torrent):
+
+@blueprint.route("/torrent/<info_hash>", methods=["GET"])
+def torrent_read(info_hash):
     """Torrent READ"""
-    return {"method": "GET", "return": torrent}
+
+    if info_hash := escape(info_hash):
+        if torrent := database.query("Torrent").filter_by(info_hash=info_hash).limit(1).first():
+            return Response.success(
+                message={
+                    "uploader": torrent.uploader,
+                    "info_hash": torrent.info_hash,
+                    "name": torrent.name,
+                    "desc": torrent.desc,
+                    "torrent_file": torrent.torrent_file,
+                    "torrent_version": torrent.torrent_version,
+                    "uploaded_time": torrent.uploaded_time,
+                    "download_count": torrent.download_count,
+                    "seeders": torrent.seeders,
+                    "leechers": torrent.leechers,
+                    "last_checked": torrent.last_checked,
+                }
+            )
+
+        return Response.error(message="Torrent unavailable.")
+
+    return Response.error(message="Torrent unavailable.")
