f0cf2b5355b290618aa251c1fb6a4b19698ce082
タジカラオ <103996576+Tajikarao@users.noreply.github.com>
2022-11-02 10:12:55 +0100

    passkey_read: refacto

--- Database/routes/passkey/read.py
+++ Database/routes/passkey/read.py
@@ -1,9 +1,30 @@
+from flask import request
+
 from utils.blueprint import Blueprint
+from utils.response import Response
+from utils.sqlalchemy import SQLAlchemy
+
+from html import escape
 
 blueprint = Blueprint("passkey_read")
+
+database: SQLAlchemy = SQLAlchemy()
 
 
 @blueprint.route("/passkey/<passKey>", methods=["GET"])
 def passkey_read(passKey):
     """Passkey READ"""
-    return {"method": "GET", "return": passKey}
+
+    if passKey := escape(passKey):
+        if passKey := database.query("PassKey").filter_by(passkey=passKey).limit(1).first():
+            return Response.success(
+                message={
+                    "passkey": passKey.passkey,
+                    "torrent": passKey.torrent,
+                    "user": passKey.user,
+                }
+            )
+
+        return Response.error(message="PassKey unavailable.")
+
+    return Response.error(message="PassKey unavailable.")
