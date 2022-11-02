9ad0d40492dddf70179ae69ce8ed2be753f977b7
タジカラオ <103996576+Tajikarao@users.noreply.github.com>
2022-11-02 13:27:41 +0100

    passkey_update: refacto

--- Database/routes/passkey/update.py
+++ Database/routes/passkey/update.py
@@ -1,9 +1,41 @@
+from html import escape
+
+from flask import request
+
 from utils.blueprint import Blueprint
+from utils.response import Response
+from utils.sqlalchemy import SQLAlchemy
 
 blueprint = Blueprint("passkey_update")
+
+database: SQLAlchemy = SQLAlchemy()
 
 
 @blueprint.route("/passkey/<passKey>", methods=["PATCH", "PUT"])
 def passkey_update(passKey):
     """Passkey UPDATE"""
-    return {"method": ["PATCH", "PUT"], "return": passKey}
+    if passKey := escape(passKey):
+        if passKey := database.query("PassKey").filter_by(passkey=passKey):
+            torrent = request.json.get("torrent", False)
+            user = request.json.get("user", 0)
+
+            if torrent:
+                passKey.update({"torrent": torrent})
+
+            if user:
+                passKey.update({"user": user})
+
+            if database.commit():
+                passKey = passKey.limit(1).first()
+
+                return Response.success(
+                    message={
+                        "passkey": passKey.passkey,
+                        "torrent": passKey.torrent,
+                        "user": passKey.user,
+                    }
+                )
+
+        return Response.error(message="PassKey unavailable.")
+
+    return Response.error(message="Error during passkey update.")
