ee4c3d2deaa0581d503964602580177f0c03a592
タジカラオ <103996576+Tajikarao@users.noreply.github.com>
2022-11-04 11:05:06 +0100

    peer_read: refacto

--- Database/routes/peer/read.py
+++ Database/routes/peer/read.py
@@ -1,9 +1,39 @@
 from utils.blueprint import Blueprint
+from utils.response import Response
+from utils.sqlalchemy import SQLAlchemy
+
+from html import escape
 
 blueprint = Blueprint("peer_read")
 
+database: SQLAlchemy = SQLAlchemy()
 
-@blueprint.route("/peer/<peer>", methods=["GET"])
-def peer_read(peer):
+
+@blueprint.route("/peer/<peer_id>", methods=["GET"])
+def torrent_read(peer_id):
     """Peer READ"""
-    return {"method": "GET", "return": peer}
+
+    if peer_id := escape(peer_id):
+        if peer := database.query("Peer").filter_by(peer_id=peer_id).limit(1).first():
+            return Response.success(
+                message={
+                    "torrent": peer.torrent,
+                    "peer_id": peer_id,
+                    "ip": peer.ip,
+                    "port": peer.port,
+                    "uploaded": peer.uploaded,
+                    "downloaded": peer.downloaded,
+                    "left": peer.left,
+                    "event": peer.event,
+                    "tracker_id": peer.tracker_id,
+                    "compact": peer.compact,
+                    "key": peer.key,
+                    "corrupt": peer.corrupt,
+                    "no_peer_id": peer.no_peer_id,
+                    "user_agent": peer.user_agent,
+                }
+            )
+
+        return Response.error(message="Peer unavailable.")
+
+    return Response.error(message="Peer unavailable.")
