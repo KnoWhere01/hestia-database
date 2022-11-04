ee9760dd31f2a6edc062cbc1751f2245b986423c
タジカラオ <103996576+Tajikarao@users.noreply.github.com>
2022-11-04 09:51:34 +0100

    peer-create: refacto

--- Database/routes/peer/create.py
+++ Database/routes/peer/create.py
@@ -1,7 +1,7 @@
 from flask import request
-
 from models.peer import Peer
 from utils.blueprint import Blueprint
+from utils.response import Response
 from utils.sqlalchemy import SQLAlchemy
 
 blueprint = Blueprint("peer_create")
@@ -13,42 +13,53 @@ database: SQLAlchemy = SQLAlchemy()
 def peer_create():
     """Peer CREATE"""
 
-    torrent = request.json.get("torrent")
-    peer_id = request.json.get("peer_id")
-    ip = request.json.get("ip")
-    port = request.json.get("port")
-    uploaded = request.json.get("uploaded")
-    downloaded = request.json.get("downloaded")
-    left = request.json.get("left")
-    event = request.json.get("event")
-    tracker_id = request.json.get("tracker_id")
-    compact = request.json.get("compact")
-    key = request.json.get("key")
-    corrupt = request.json.get("corrupt")
-    no_peer_id = request.json.get("no_peer_id")
-    user_agent = request.json.get("user_agent")
+    torrent = request.json.get("torrent", False)
+    peer_id = request.json.get("peer_id", False)
+    ip = request.json.get("ip", False)
+    port = request.json.get("port", False)
+    uploaded, downloaded, left = 0, 0, 0
+    event = request.json.get("event", False)
+    tracker_id = request.json.get("tracker_id", False)
+    compact = request.json.get("compact", False)
+    key = request.json.get("key", False)
+    corrupt = request.json.get("corrupt", False)
+    no_peer_id = request.json.get("no_peer_id", False)
+    user_agent = request.json.get("user_agent", False)
 
-    peer = Peer(
-        torrent=torrent,
-        peer_id=peer_id,
-        ip=ip,
-        port=port,
-        uploaded=uploaded,
-        downloaded=downloaded,
-        left=left,
-        event=event,
-        tracker_id=tracker_id,
-        compact=compact,
-        key=key,
-        corrupt=corrupt,
-        no_peer_id=no_peer_id,
-        user_agent=user_agent,
-    )
+    if (
+        torrent
+        and peer_id
+        and ip
+        and port
+        and event
+        and tracker_id
+        and compact
+        and key
+        and corrupt
+        and no_peer_id
+        and user_agent
+    ):
+        if not (database.query("Peer").filter_by(peer_id=peer_id).limit(1).first()):
+            peer = Peer(
+                torrent=torrent,
+                peer_id=peer_id,
+                ip=ip,
+                port=port,
+                uploaded=uploaded,
+                downloaded=downloaded,
+                left=left,
+                event=event,
+                tracker_id=tracker_id,
+                compact=compact,
+                key=key,
+                corrupt=corrupt,
+                no_peer_id=no_peer_id,
+                user_agent=user_agent,
+            )
 
-    if database.add(peer) and database.commit():
-        return {
-            "succes": True,
-            "peer": "",
-        }, 200
+            if database.add(peer) and database.commit():
+                return Response.success(message="Peer created successfully.")
 
-    return {"succes": False}, 403
+        return Response.error(message="Peer unavailable.")
+
+    return Response.error(message="Error during peer creation.")
