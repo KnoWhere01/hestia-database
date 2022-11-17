be820dd9eaf25e143bb5a4d7d6cc61152aaf82e7
タジカラオ <103996576+Tajikarao@users.noreply.github.com>
2022-11-17 15:15:46 +0100

    models.user: add api_key

--- Database/models/user.py
+++ Database/models/user.py
@@ -8,11 +8,12 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     username = Column(String(255), unique=True)
+    api_key = Column(String(36), unique=True)
     uploaded = Column(BIGINT(unsigned=True), default=0)
     downloaded = Column(BIGINT(unsigned=True), default=0)
 
     def __repr__(self):
         return (
-            "<User(username='%s', uploaded='%s', downloaded='%s')>"
-            % (self.username, self.uploaded, self.downloaded)
+            "<User(username='%s', api_key='%s', uploaded='%s', downloaded='%s')>"
+            % (self.username, self.api_key, self.uploaded, self.downloaded)
         )
