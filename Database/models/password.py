4f91a001b57d96d821d230ce87bdb58f851f6f9b
タジカラオ <103996576+Tajikarao@users.noreply.github.com>
2022-11-17 11:42:11 +0100

    add models.password

---
+++ Database/models/password.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.dialects.mysql import BIGINT
+
+from utils.base import Base
+
+
+class Password(Base):
+    __tablename__ = "passwords"
+    id = Column(Integer, primary_key=True)
+    user = Column(BIGINT(unsigned=True), nullable=False)
+    password = Column(String(60), nullable=False)
+
+    def __repr__(self):
+        return (
+            "<Password(user='%s', password='%s')>"
+            % (
+                self.user,
+                self.password,
+            )
+        )
